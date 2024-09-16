a6c46d8ff578c8e9175812f0618442b5bdbb8475
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2024-09-16 01:36:12 -0700

    Initialize VitePress
    - Add theme submodule
    - Add initial configuration files
    - Override theme-default VPHero/VPFooter with custom theme version
    - Add configuration for tabs
    - Update README

--- README.md
+++ README.md
@@ -7,8 +7,7 @@ https://wii.hacks.guide/
 ## Running the site locally
 
 This requires the following installed on your system:
-- ruby(-dev)
-- bundler
+- node.js
 
 To test the website locally, clone the source code:
 
@@ -20,9 +19,8 @@ cd Guide_Wii
 Then simply run the following commands:
 
 ```sh
-bundle config set --local path vendor/bundle
-bundle install
-bundle exec jekyll serve
+npm ci
+npm run docs:dev
 ```
 
-The website should now be running on http://127.0.0.1:4000/.
+The website should now be running on http://127.0.0.1:5173/ (or a port shown on the terminal).
---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,57 @@
+/*
+  Copyright (C) 2024 Nintendo Homebrew
+  SPDX-License-Identifier: MIT
+*/
+
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vitepress'
+
+import container from 'markdown-it-container'
+
+export default defineConfig({
+  title: "Wii Hacks Guide",
+  description: "The complete guide to modding your Wii, vWii, and Wii mini",
+  vite: {
+    resolve: {
+      alias: [
+        {
+          find: /^.*\/VPHero\.vue$/,
+          replacement: fileURLToPath(
+            new URL('./theme/components/VPHero.vue', import.meta.url)
+          )
+        },
+        {
+          find: /^.*\/VPFooter\.vue$/,
+          replacement: fileURLToPath(
+            new URL('./theme/components/VPFooter.vue', import.meta.url)
+          )
+        }
+      ]
+    }
+  },
+  markdown: {
+    config: (md) => {
+      md.use(container, "tabs", {
+        render: (tokens, idx) => {
+          const token = tokens[idx];
+          if (token.nesting === 1) {
+            return `<Tabs ${token.info}>\n`;
+          } else {
+            return `</Tabs>\n`;
+          }
+        }
+      });
+
+      md.use(container, 'tab', {
+        render: (tokens, idx) => {
+          const token = tokens[idx];
+          if (token.nesting === 1) {
+            return `<Tab name="${token.info.match(/^ ?tab\s+(.*)$/)[1]}">`;
+          } else {
+            return `</Tab>\n`;
+          }
+        }
+      });
+    }
+  }
+})
