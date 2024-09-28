730e1e7c437afe5ef3b2ee577cce272efa7f7053
lifehackerhansol <lifehackerhansol@ds-homebrew.com>
2024-09-28 14:57:37 -0700

    clean_translations: update for VitePress

--- clean_translations.py
+++ clean_translations.py
@@ -4,13 +4,16 @@ import os
 files_in_main = []
 
 
-for i in os.listdir("_pages/en_US/"):
-    files_in_main.append(i)
+for i in os.listdir("docs"):
+    if i.endswith(".md"):
+        files_in_main.append(i)
 
-for i in os.listdir("_pages/"):
-    if i == "en_US":
+for i in os.listdir("docs"):
+    if not os.path.isdir(f"docs/{i}"):
         continue
-    for j in os.listdir(f"_pages/{i}"):
+    if i in [".vitepress", "public", "_include"]:
+        continue
+    for j in os.listdir(f"docs/{i}"):
         if j not in files_in_main:
-            print(f"Deleting _pages/{i}/{j}")
-            os.remove(f"_pages/{i}/{j}")
+            print(f"Deleting docs/{i}/{j}")
+            os.remove(f"docs/{i}/{j}")
